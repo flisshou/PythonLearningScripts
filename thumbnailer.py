084ede4db793fa6758927e06d77d70b3e94a1782
flisshou <fliss_0104127723@hotmail.com>
2017-05-02 21:09:22 +0800

    Fix some weirdness but not done yet

--- thumbnailer.py
+++ thumbnailer.py
@@ -30,6 +30,7 @@ class RemoteControl:
         try:
             print('>>> Connecting to {}...'.format(addr))
             self.ssh.connect(addr, username=username, password=password, timeout=10)
+            self.ssh.exec_command('PATH=$PATH:/usr/local/bin')
 
         except socket.error:
             print('>>> Could not connect to remote device - {}\n'.format(addr))
@@ -44,7 +45,7 @@ class RemoteControl:
 
         stdin, stdout, stderr = self.ssh.exec_command(command)
         video_names = [f.rstrip() for f in stdout]
-
+        
         if len(video_names):
             for v in video_names:
                 if '.mp4' in v:
@@ -61,7 +62,7 @@ class RemoteControl:
     def scp_remote_video(self, remote_video_path):
         local_video_path = '/tmp/CopiedVideo.mp4'
         local_thumbnail_path = '{}/{}'.format(args.d, self.thname)
-
+        print('local_thumbnail_path = {}'.format(local_thumbnail_path))
         if self.ssh:
             self.scp = SCPClient(self.ssh.get_transport())
             self.scp.get(remote_path=remote_video_path, local_path=local_video_path)
@@ -69,7 +70,7 @@ class RemoteControl:
             FFmpegParser(video_path=local_video_path, thumbnail_path=local_thumbnail_path)
 
     def write_message(self):
-        msg = '{} {} ==> {}/{}'.format(self.addr, self.vname, self.local_thumbnail_dir, self.thname)
+        msg = '{} {} ==> {}/{}'.format(self.addr, self.vname, args.d, self.thname)
         print(msg)
 
 
@@ -78,8 +79,9 @@ class FFmpegParser:
     Use simple ffmpeg command to screenshot the video(s) from ssh/scp processes
     """
 
-    def __init__(self, video_path, thumbnail_path, second=15):
-        command = 'ffmpeg -i {} -ss 00:00:{}.000 -vframes 1 {}'.format(video_path, second, thumbnail_path)
+    def __init__(self, video_path, thumbnail_path):
+        command = 'ffmpeg -i {} -ss 00:00:00.000 -vframes 1 {}'.format(video_path, thumbnail_path)
+        print(command)
         subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 
@@ -101,7 +103,6 @@ def get_account(addr):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A script for generating thumbnails.')
     parser.add_argument('source', action='store', help='source directory')
-    parser.add_argument('-t', action='append', help='the GMT timeframe(s); <YYYYMMDD-HH>')
     parser.add_argument('-a', action='append', help='ip address of device(s)')
     parser.add_argument('-d', action='store', help='output directory', default='tmp')
 
